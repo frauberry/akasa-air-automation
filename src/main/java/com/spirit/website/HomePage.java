dda330317ea13710598007d00a42ba3b63dbdcbb
frauberry <olgasachkovasqa@gmail.com>
2025-07-13 23:07:54 -0400

    add sign up scenario

--- src/main/java/com/spirit/website/HomePage.java
+++ src/main/java/com/spirit/website/HomePage.java
@@ -5,8 +5,51 @@ import org.openqa.selenium.By;
 import static com.spirit.utils.DriverWeb.*;
 
 public class HomePage {
+    By loginButton = By.xpath("//button[text()='Login']");
+    By customerLogin = By.xpath("//div[text()='Customer Login']");
+    By signUpButton = By.xpath("//button[text()='Sign Up']");
+    By firstName = By.xpath("//input[@name='firstName']");
+    By lastName = By.xpath("//input[@name='lastName']");
+    By gender = By.xpath("//input[@name='gender']/parent::*");
+    By genderFemale = By.xpath("//li[text()='Female']");
+    By dateOfBirth = By.xpath("//input[@placeholder='DD MMMM YYYY']");
+    By emailAddress = By.xpath("//input[@name='emailID']");
+    By phoneNumber = By.xpath("//input[@name='mobile']");
+    By submitButton = By.xpath("//button[text()='Submit']");
+    By password = By.xpath("//input[@name='password']");
+    By confirmPassword = By.xpath("//input[@name='confirmPassword']");
+    By confirmSignInButton = By.xpath("//button[text()='Confirm & Sign In']");
+    By accountName = By.xpath("//span[text()='Tita Rita']");
 
-    public void test(){
-        clickOnElement(By.xpath(""));
+    public void enterCredentials() {
+        hoverElement(loginButton);
+//        getDriver().findElement(loginButton).click();
+        getDriver().findElement(customerLogin).click();
+        getDriver().findElement(signUpButton).click();
+        getDriver().findElement(firstName).sendKeys("Tita");
+        getDriver().findElement(lastName).sendKeys("Rita");
+        getDriver().findElement(gender).click();
+        getDriver().findElement(genderFemale).click();
+        getDriver().findElement(dateOfBirth).click();
+        getDriver().findElement(dateOfBirth).sendKeys("01N1974");
+        getDriver().findElement(emailAddress).sendKeys("test89899@gmail.com");
+        getDriver().findElement(phoneNumber).sendKeys("1112345678");
+        getDriver().findElement(submitButton).click();
+        waitForElementToBeVisible(password,5);
+        getDriver().findElement(password).sendKeys("Spirit1!");
+//        waitForElementToBeClickable(confirmPassword,5);
+        sleep(2);
+        getDriver().findElement(confirmPassword).sendKeys("Spirit1!");
+
+
+    }
+
+    public void signIn () {
+        getDriver().findElement(confirmSignInButton).click();
+    }
+
+    public boolean isAccountNameDisplayed () {
+        waitForElementToBeVisible(accountName,10);
+        return getDriver().findElement(accountName).isDisplayed();
     }
 }
--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -1,9 +1,12 @@
 package steps;
 
+import com.spirit.website.HomePage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import com.spirit.utils.Logger;
+import org.testng.Assert;
 
 import static com.spirit.utils.DriverWeb.getDriver;
 
@@ -26,5 +29,35 @@ public class StepDefinitions {
     public void correspondingPageShouldBeDisplayed() {
         Logger.setThenStep("Verifying the corresponding page...");
         Logger.screenshotPass("Page verified successfully.");
+    }
+
+    @Given("Member is on Homepage")
+    public void memberIsOnHomepage() {
+        Logger.setGivenStep("Member is on Homepage");
+        getDriver().get("https://www.akasaair.com/");
+        Logger.screenshotInfo("Member is on Homepage");
+    }
+
+    @When("Member enters valid credentials")
+    public void memberEntersValidCredentials() {
+        Logger.setWhenStep("Member enters valid credentials");
+        HomePage homepage = new HomePage();
+        homepage.enterCredentials();
+        Logger.screenshotInfo("Credentials are entered");
+    }
+
+    @And("Member signs in")
+    public void memberSignsIn() {
+        Logger.setAndStep("Member signs in");
+        HomePage homePage = new HomePage();
+        homePage.signIn();
+        Logger.screenshotInfo("Member is signed in");
+    }
+
+    @Then("Member should be successfully signed in")
+    public void memberShouldBeSuccessfullySignedIn() {
+        Logger.setThenStep("Member should be successfully signed in");
+        HomePage homePage = new HomePage();
+        Assert.assertTrue(homePage.isAccountNameDisplayed());
     }
 }
