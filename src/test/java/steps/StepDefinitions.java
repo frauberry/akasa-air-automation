f2dedad93bb8a65ce595a1ec1ce37457157c288d
frauberry <olgasachkovasqa@gmail.com>
2025-07-13 23:12:20 -0400

    remove unused code

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -11,26 +11,6 @@ import org.testng.Assert;
 import static com.spirit.utils.DriverWeb.getDriver;
 
 public class StepDefinitions {
-
-    @Given("user is on the home page")
-    public void userIsOnTheHomePage() {
-        Logger.setGivenStep("Navigating to home page...");
-        getDriver().get("https://www.spirit.com");
-        Logger.screenshotInfo("User navigated to the home page");
-    }
-
-    @When("user navigates thru all the options")
-    public void userNavigatesThruALLTheOptions() {
-        Logger.setWhenStep("User is navigating through all the options...");
-        Logger.screenshotInfo("User navigated through the options.");
-    }
-
-    @Then("corresponding page should be displayed")
-    public void correspondingPageShouldBeDisplayed() {
-        Logger.setThenStep("Verifying the corresponding page...");
-        Logger.screenshotPass("Page verified successfully.");
-    }
-
     @Given("Member is on Homepage")
     public void memberIsOnHomepage() {
         Logger.setGivenStep("Member is on Homepage");
